19d40608806c421709b354dad85e5a928531f730
Paul Stone <paul@pdjs.co.uk>
2022-03-25 22:42:29 +0000

    Load floppy disk from URL

---
+++ web/ui.js
@@ -0,0 +1,138 @@
+var statusElement = document.getElementById('status');
+var progressElement = document.getElementById('progress');
+var spinnerElement = document.getElementById('spinner');
+
+var Module = {
+  preRun: [],
+  postRun: [],
+
+  print: (function() {
+    var element = document.getElementById('output');
+    if (element) element.value = ''; // clear browser cache
+    return function(text) {
+      if (arguments.length > 1) text = Array.prototype.slice.call(arguments).join(' ');
+      // These replacements are necessary if you render to raw HTML
+      //text = text.replace(/&/g, "&amp;");
+      //text = text.replace(/</g, "&lt;");
+      //text = text.replace(/>/g, "&gt;");
+      //text = text.replace('\n', '<br>', 'g');
+      console.log(text);
+      if (element) {
+        element.value += text + "\n";
+        element.scrollTop = element.scrollHeight; // focus on bottom
+      }
+    };
+  })(),
+
+  canvas: (function() {
+    var canvas = document.getElementById('canvas');
+
+    // As a default initial behavior, pop up an alert when webgl context is lost. To make your
+    // application robust, you may want to override this behavior before shipping!
+    // See http://www.khronos.org/registry/webgl/specs/latest/1.0/#5.15.2
+    canvas.addEventListener("webglcontextlost", function(e) { alert('WebGL context lost. You will need to reload the page.'); e.preventDefault(); }, false);
+
+    return canvas;
+  })(),
+
+  setStatus: function(text) {
+    if (!Module.setStatus.last) Module.setStatus.last = { time: Date.now(), text: '' };
+    if (text === Module.setStatus.last.text) return;
+    var m = text.match(/([^(]+)\((\d+(\.\d+)?)\/(\d+)\)/);
+    var now = Date.now();
+    if (m && now - Module.setStatus.last.time < 30) return; // if this is a progress update, skip it if too soon
+    Module.setStatus.last.time = now;
+    Module.setStatus.last.text = text;
+    if (m) {
+      text = m[1];
+      progressElement.value = parseInt(m[2])*100;
+      progressElement.max = parseInt(m[4])*100;
+      progressElement.hidden = false;
+      spinnerElement.hidden = false;
+    } else {
+      progressElement.value = null;
+      progressElement.max = null;
+      progressElement.hidden = true;
+      if (!text) spinnerElement.style.display = 'none';
+    }
+    statusElement.innerHTML = text;
+  },
+
+  totalDependencies: 0,
+
+  monitorRunDependencies: function(left) {
+    this.totalDependencies = Math.max(this.totalDependencies, left);
+    Module.setStatus(left ? 'Preparing... (' + (this.totalDependencies-left) + '/' + this.totalDependencies + ')' : 'All downloads complete.');
+  }
+
+};
+
+Module.setStatus('Downloading...');
+
+window.onerror = function(event) {
+    // TODO: do not warn on ok events like simulating an infinite loop or exitStatus
+    Module.setStatus('Exception thrown, see JavaScript console');
+    spinnerElement.style.display = 'none';
+    Module.setStatus = function(text) {
+    if (text) Module.printErr('[post-exception status] ' + text);
+    };
+};
+
+let currentDiskFile = null;
+
+async function loadDisk(url) {
+    if (url == "") return;
+    let response = await fetch(url);
+    let buf = await response.arrayBuffer();
+    let data = new Uint8Array(buf);
+
+    if (currentDiskFile) {
+    ccall('arc_disc_eject', null, ['number'], [0]);
+    FS.unlink(currentDiskFile);
+    }
+    currentDiskFile = url.substr(url.lastIndexOf('/')+1);
+    FS.createDataFile("/", currentDiskFile, data, true, true);
+    ccall('arc_disc_change', null, ['number', 'string'], [0, '/' + currentDiskFile]);
+}
+
+async function* makeTextFileLineIterator(fileURL) {
+    const utf8Decoder = new TextDecoder('utf-8');
+    const response = await fetch(fileURL);
+    const reader = response.body.getReader();
+    let { value: chunk, done: readerDone } = await reader.read();
+    chunk = chunk ? utf8Decoder.decode(chunk) : '';
+
+    const re = /\n|\r|\r\n/gm;
+    let startIndex = 0;
+    let result;
+
+    for (;;) {
+        let result = re.exec(chunk);
+        if (!result) {
+        if (readerDone) {
+            break;
+        }
+        let remainder = chunk.substr(startIndex);
+        ({ value: chunk, done: readerDone } = await reader.read());
+        chunk = remainder + (chunk ? utf8Decoder.decode(chunk) : '');
+        startIndex = re.lastIndex = 0;
+        continue;
+        }
+        yield chunk.substring(startIndex, result.index);
+        startIndex = re.lastIndex;
+    }
+    if (startIndex < chunk.length) {
+        // last line didn't end in a newline char
+        yield chunk.substr(startIndex);
+    }
+}
+
+async function setupDisks() {
+    let dropdown = document.getElementById('disks');
+    for await (let diskUrl of makeTextFileLineIterator('disk_index.txt')) {
+        let diskName = diskUrl.substr(diskUrl.lastIndexOf('/')+1);
+        dropdown.add(new Option(diskName, diskUrl));
+    }
+}
+
+setupDisks();
