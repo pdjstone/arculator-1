f4e475c2a0df8fb6c424f3d75378481f9016afc4
Paul Stone <pdjstone@users.noreply.github.com>
2023-09-25 14:08:09 +0100

    Allow Ctrl+Right Click to get popup menu in absolute mouse mode

--- src/wx-sdl2.c
+++ src/wx-sdl2.c
@@ -92,14 +92,14 @@ static int arc_main_thread(void *p)
 //                                quited = 1;
 				arc_stop_emulation();
 			}
-			if (e.type == SDL_MOUSEBUTTONUP && !mousecapture && mouse_mode == MOUSE_MODE_RELATIVE)
+			if (e.type == SDL_MOUSEBUTTONUP && !mousecapture)
 			{
-				if (e.button.button == SDL_BUTTON_LEFT)
+				if (e.button.button == SDL_BUTTON_LEFT && mouse_mode == MOUSE_MODE_RELATIVE)
 				{
 					rpclog("Mouse click -- enabling mouse capture\n");
 					sdl_enable_mouse_capture();
 				}
-				else if (e.button.button == SDL_BUTTON_RIGHT)
+				else if (e.button.button == SDL_BUTTON_RIGHT && (mouse_mode == MOUSE_MODE_RELATIVE || key[KEY_LCONTROL] || key[KEY_RCONTROL]))
 				{
 					arc_popup_menu();
 				}
