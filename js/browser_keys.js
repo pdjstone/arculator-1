0281bb2f9a347f9d48cb18385dcd99ba436f6b8f
Paul Stone <pdjstone@users.noreply.github.com>
2024-11-12 13:55:33 +0000

    Remove some logging

--- js/browser_keys.js
+++ js/browser_keys.js
@@ -2,7 +2,7 @@
  * Called from keyboard_init in keyboard.c
  */
 function browser_init_keys(keystate, keylayout) {
-    console.log('browser_init_keys', keystate, keylayout);
+    //console.log('browser_init_keys', keystate, keylayout);
     getEmuInput().setupArculatorKeyboard(keystate, keylayout);
 }
 
@@ -10,7 +10,7 @@ function browser_init_keys(keystate, keylayout) {
  * Called from mouse_init in input_emscripten.c
  */
 function browser_init_mouse(mxPtr, myPtr, buttonsPtr, mouseModePtr) { 
-    console.log('browser_init_mouse', mxPtr, myPtr, mouseModePtr);
+    //console.log('browser_init_mouse', mxPtr, myPtr, mouseModePtr);
     getEmuInput().setupArculatorMouse(buttonsPtr, mxPtr, myPtr, mouseModePtr);
 }
 
